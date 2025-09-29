89e74c43b8dc6e65b480444c1823ca022c506e1c
fscorsini <fscorsini@gmail.com>
2025-09-29 20:11:45 -0300

    Código da animacao player finalizado

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var anim = $anim as AnimatedSprite2D
+var jumpPlayer = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		jumpPlayer = true
+	else:
+		jumpPlayer = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
@@ -17,9 +21,19 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	#print(direction)
 	if direction:
+		anim.scale.x = direction
 		velocity.x = direction * SPEED
+		if !jumpPlayer:
+			anim.play("run")
+		else:
+			anim.play("jump")
 	else:
+		if !jumpPlayer:
+			anim.play("idle")
+		else:
+			anim.play("jump")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if position.y >= 400:
